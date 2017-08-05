1ecca0f566331f39318fccbe2337f217427fa891
Fellzo <gely902@gmail.com>
2017-08-05 15:53:35 +0300

    Change functions

--- pprint_json.py
+++ pprint_json.py
@@ -10,35 +10,44 @@ def load_data(filepath):
     return json.loads(''.join(json_file.readlines()))
 
 
-def pretty_print_json(data):
-    def prettify(current_object, tabs=0, first_token_tabs=True, has_next=True):
-        open_token = '{' if type(current_object) == dict else '['
-        close_token = '}' if type(current_object) == dict else ']'
-        if first_token_tabs:
-            print('\t' * tabs + open_token)
-        else:
-            print(open_token)
-        # Показывает сколько ключей из объекта уже были отработаны, нужно для постановки запятой
-        iterated = 0
-        for key in current_object:
-            iterated += 1
-            if type(current_object) == dict:
-                print('\t' * (tabs + 1) + key + ': ', end='')
-                if type(current_object[key]) == dict or type(current_object[key]) == list:
-                    prettify(current_object[key], tabs + 1, False, len(current_object) - iterated != 0)
-                else:
-                    print(str(current_object[key]) + ',' * min(1, len(current_object) - iterated))
+def __get_tokens_for_object(object_type):
+    return '{' if object_type == dict else '[', '}' if object_type == dict else ']'
+
+
+def __is_iterable(object_type):
+    return object_type == list or object_type == dict
+
+
+def __prettify(current_object, tabs=0, first_token_tabs=True, has_next=True):
+    open_token, close_token = __get_tokens_for_object(type(object))
+    if first_token_tabs:
+        print('\t' * tabs + open_token)
+    else:
+        print(open_token)
+    # Показывает сколько ключей из объекта уже были отработаны, нужно для постановки запятой
+    iterated = 0
+    for key in current_object:
+        iterated += 1
+        keys_left = len(current_object) - iterated
+        if type(current_object) == dict:
+            print('\t' * (tabs + 1) + key + ': ', end='')
+            if __is_iterable(type(current_object[key])):
+                __prettify(current_object[key], tabs + 1, False, keys_left != 0)
             else:
-                if type(key) == dict or type(key) == dict:
-
-                    prettify(key, tabs + 1, True, len(current_object) - iterated != 0)
-        print('\t' * tabs + close_token, end='')
-        if has_next:
-            print(',')
+                print(str(current_object[key]) + ',' * min(1, keys_left))
         else:
-            print()
+            if __is_iterable(type(key)):
+                __prettify(key, tabs + 1, True, keys_left != 0)
+    print('\t' * tabs + close_token, end='')
+    if has_next:
+        print(',')
+    else:
+        print()
 
-    prettify(data, 0, False, False)
+
+def pretty_print_json(json_input_data):
+    __prettify(json_input_data, 0, False, False)
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
